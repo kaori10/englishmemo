55ba48caa33c7ba2fab10b174c70f2550c43ef8b
vagrant <vagrant@localhost.localdomain>
2020-11-18 03:35:33 +0000

    add new

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,21 @@
+class BlogsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  	@blog = Blog.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+end
